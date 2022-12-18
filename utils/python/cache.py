3211556c3e877d570add021e2d6832da69e250a8
silhavyj <jakub.sil@seznam.cz>
2022-12-18 13:53:51 +0100

    backup

--- utils/python/cache.py
+++ utils/python/cache.py
@@ -1,39 +1,35 @@
 import sys
 import requests
 
+
+def print_help():
+    print('GET:    python3 cache.py <ip_addr> get <key>')
+    print('PUT:    python3 cache.py <ip_addr> put <key> <value>')
+    print('DELETE: python3 cache.py <ip_addr> delete <key>')
+
+
 if __name__ == '__main__':
-    #argc = len(sys.argv)
-    #if argc != 3:
-    #    print("Invalid number of arguments")
-    
-    argv = sys.argv
-    ip_addr = argv[1]
-    op = argv[2].lower()
-    
-    if op == 'put':
+    try:
+        argv = sys.argv
+        ip_addr = argv[1]
+        op = argv[2].lower()
         key = argv[3]
-        value = argv[4]
-        try:
-            response = requests.put(f'http://{ip_addr}:5000/data?key={key}&value={value}')
-            print(f'status_code: {response.status_code}')
-            print(f'data: {response.text}')
-        except Exception as e:
-            print(e)
-    elif op == 'del':
-        key = argv[3]
-        try:
-            response = requests.delete(f'http://{ip_addr}:5000/data?key={key}')
-            print(f'status_code: {response.status_code}')
-            print(f'data: {response.text}')
-        except Exception as e:
-            print(e)
-    elif op == 'get':
-        key = argv[3]
-        try:
+        
+        if op == 'get':
             response = requests.get(f'http://{ip_addr}:5000/data?key={key}')
-            print(f'status_code: {response.status_code}')
-            print(f'data: {response.text}')
-        except Exception as e:
-            print(e)
-    else:
-        print('invalid operation entered')
+        elif op == 'put':
+            value = argv[4]
+            response = requests.put(f'http://{ip_addr}:5000/data?key={key}&value={value}')
+        elif op == 'delete':
+            response = requests.delete(f'http://{ip_addr}:5000/data?key={key}')
+        else:
+            print("Invalid arguments")
+            print_help()
+            exit()
+            
+        print(f'status_code: {response.status_code}')
+        print(f'data: {response.text.strip()}')
+    except Exception as e:
+        print(e)
+        print()
+        print_help()
